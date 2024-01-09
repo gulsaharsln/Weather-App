c023a4f4c693008a7831251d8269bfab422acf86
begumayten <79323462+begumayten@users.noreply.github.com>
2024-01-09 13:31:57 +0300

    navigation error solved, build updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,7 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation ("androidx.navigation:navigation-compose:2.4.0")
 
     // Jetpack Compose dependencies
     implementation("androidx.activity:activity-compose:1.8.2")
